b2639696468c8130512580543a10aff96ff57b7a
Hoang Dung <dunghq87@gmail.com>
2016-05-30 07:35:38 +0700

    V 0.1

--- application/views/header.php
+++ application/views/header.php
@@ -1,19 +1,3 @@
-<!-- mp-menu -->
-<nav id="mp-menu" class="mp-menu">
-    <div class="mp-level">
-        <ul>
-            <li class="<?php if($page=='') echo 'active';?>"><a href="<?php echo base_url();?>">Trang chủ</a></li>
-            <li class="<?php if($page=='about') echo 'active';?>"><a href="<?php echo base_url('ve-chung-toi');?>">Giới thiệu</a></li>
-            <li class="<?php if($page=='product') echo 'active';?>"><a href="<?php echo base_url('danh-muc-san-pham');?>">Sản phẩm</a></li>
-            <li class="<?php if($page=='news') echo 'active';?>"><a href="<?php echo base_url('tin-tuc');?>">Tin tức</a></li>
-            <li class="<?php if($page=='contact') echo 'active';?>"><a href="<?php echo base_url('lien-he');?>">Liên hệ</a></li>
-            <li class="<?php if($page=='gallery') echo 'active';?>"><a href="<?php echo base_url('thu-vien-anh');?>">Ảnh ọt</a></li>
-        </ul>
-
-    </div>
-</nav>
-<!-- /mp-menu -->
-
 <div id="top" class="std">&nbsp;</div>
 <div id="header">
     <div id="header_content">
@@ -23,23 +7,45 @@
         </div>
         <div id="login_area">
             <div id="button_login"><a href="">Đăng nhập</a>&nbsp;/&nbsp;<a href="">Đăng ký</a></div>
-        </div>
-        <div id="search_area">
-            <input type="text" placeholder="tìm kiếm"/>
         </div> -->
-        <a href="#" id="trigger" class="btn-toogle-res-menu"></a>
-        <div class="clear"></div>
-    </div>
-</div>
-<div id="menu">
-    <div id="menu_content">
-        <ul>
-            <li class="<?php if($page=='') echo 'active';?>"><a href="<?php echo base_url();?>">Trang chủ</a></li>
-            <li class="<?php if($page=='about') echo 'active';?>"><a href="<?php echo base_url('ve-chung-toi');?>">Giới thiệu</a></li>
-            <li class="<?php if($page=='product') echo 'active';?>"><a href="<?php echo base_url('danh-muc-san-pham');?>">Sản phẩm</a></li>
-            <li class="<?php if($page=='news') echo 'active';?>"><a href="<?php echo base_url('tin-tuc');?>">Tin tức</a></li>
-            <li class="<?php if($page=='contact') echo 'active';?>"><a href="<?php echo base_url('lien-he');?>">Liên hệ</a></li>
-            <li class="<?php if($page=='gallery') echo 'active';?>"><a href="<?php echo base_url('thu-vien-anh');?>">Ảnh ọt</a></li>
+        <form class="navbar-form navbar-left" method="get" action="<?php echo base_url();?>danh-muc-san-pham/">
+            <div id="search-area" class="input-group">
+                <div class="input-group-btn search-panel">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="margin-right: -2px;" aria-expanded="false"> 
+                            All
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <?php if($list_categories){
+                            foreach ($list_categories as $key => $value) {?>
+                                <li class="<?php if($page=='about') echo 'active';?>"><a href="<?php echo base_url("danh-muc-san-pham/".$value['slug'].'/'.$value['id']); ?>"><?php echo $value['name']?></a></li>
+                        <?php }
+                            }?>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <input type="text" class="form-control" name="txtSearch" placeholder="Tìm kiếm" value="<?php echo $txtSearch; ?>">
+                <span class="input-group-btn">
+                    <button id="plsWaitSearchBtn" class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                </span>
+            </div>
+        </form>
+        <ul class="nav navbar-nav navbar-right text-uppercase">
+            <li class="dropdown shellOff">
+                <a href="#" class="dropdown-toggle tpnav " data-toggle="dropdown" role="button" aria-expanded="false">
+                <span class="hidden-sm cart-checkoutHide">Danh mục <span class="caret"></span></span>
+                <span style="font-size:27px;" class="visible-sm cart-checkoutHide"><i class="fa fa-bars cart-checkoutHide"></i></span>
+                </a>
+                <ul class="featured_menu dropdown-menu" role="menu">
+                    <?php if($list_categories){
+                        foreach ($list_categories as $key => $value) {?>
+                            <li class="<?php if($page=='about') echo 'active';?>"><a href="<?php echo base_url("danh-muc-san-pham/".$value['slug'].'/'.$value['id']); ?>"><?php echo $value['name']?></a></li>
+                    <?php }
+                        }?>
+                </ul>
+            </li>
+            <li class="shellOff"><a href="<?php echo base_url();?>lien-he">Liên hệ</a></li>
         </ul>
+        <div class="clear"></div>
     </div>
 </div>
